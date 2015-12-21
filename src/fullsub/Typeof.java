7a1707cbbc9b643be4232da3fb3521be6cb3524d
Weixin Zhang <zhangweixinxd@gmail.com>
2015-12-21 15:03:32 +0800

    Typer for fullsub

--- src/fullsub/Typeof.java
+++ src/fullsub/Typeof.java
@@ -1,10 +1,13 @@
 package fullsub;
 
-import moreextension.TyEqv;
-import moreextension.tyalg.external.TyAlgMatcher;
-import moreextension.tyalg.shared.GTyAlg;
+import java.util.function.Function;
 
-public interface Typeof<Term, Ty, Bind> extends moreextension.Typeof<Term, Ty, Bind>, typed.Typeof<Term, Ty, Bind> {
+import fullsub.termalg.shared.GTermAlg;
+import fullsub.tyalg.external.TyAlgMatcher;
+import fullsub.tyalg.shared.GTyAlg;
+import utils.Context;
+
+public interface Typeof<Term, Ty, Bind> extends GTermAlg<Term, Ty, Function<Context<Bind>, Ty>>, moreextension.Typeof<Term, Ty, Bind>, top.Typeof<Term, Ty, Bind> {
 	@Override
 	TyEqv<Ty> tyEqv();
 
@@ -13,4 +16,60 @@ public interface Typeof<Term, Ty, Bind> extends moreextension.Typeof<Term, Ty, B
 
 	@Override
 	GTyAlg<Ty, Ty> tyAlg();
+
+	Subtype<Ty> subtype();
+	JoinMeet<Ty> joinMeet();
+
+	@Override
+	default Function<Context<Bind>, Ty> TmIf(Term t1, Term t2, Term t3) {
+		return ctx -> {
+			Ty ty1 = visitTerm(t1).apply(ctx);
+			if (subtype().subtype(ty1, tyAlg().TyBool())) {
+				Ty ty2 = visitTerm(t2).apply(ctx);
+				Ty ty3 = visitTerm(t3).apply(ctx);
+				return joinMeet().join(ty2, ty3);
+			}
+			return m().empty().apply(ctx);
+		};
+	}
+
+	@Override
+	default Function<Context<Bind>, Ty> TmFix(Term t) {
+		return ctx -> {
+			Ty ty = visitTerm(t).apply(ctx);
+			return tyMatcher()
+					.TyArr(ty1 -> ty2 -> subtype().subtype(ty1, ty2) ? ty2 : m().empty().apply(ctx))
+					.otherwise(() -> m().empty().apply(ctx))
+					.visitTy(ty);
+		};
+	}
+
+	@Override
+	default Function<Context<Bind>, Ty> TmTimesFloat(Term t1, Term t2) {
+		Ty tyFloat = tyAlg().TyFloat();
+		return ctx -> subtype().subtype(visitTerm(t1).apply(ctx), tyFloat) &&
+				subtype().subtype(visitTerm(t2).apply(ctx), tyFloat) ? tyFloat : m().empty().apply(ctx);
+	}
+
+	@Override
+	default Function<Context<Bind>, Ty> TmAscribe(Term t, Ty ty) {
+		return ctx -> subtype().subtype(visitTerm(t).apply(ctx), ty) ? ty : m().empty().apply(ctx);
+	}
+
+	@Override
+	default Function<Context<Bind>, Ty> TmSucc(Term t) {
+		Ty tyNat = tyAlg().TyNat();
+		return ctx -> subtype().subtype(visitTerm(t).apply(ctx), tyNat) ? tyNat : m().empty().apply(ctx);
+	}
+
+	@Override
+	default Function<Context<Bind>, Ty> TmPred(Term t) {
+		Ty tyNat = tyAlg().TyNat();
+		return ctx -> subtype().subtype(visitTerm(t).apply(ctx), tyNat) ? tyNat : m().empty().apply(ctx);
+	}
+
+	@Override
+	default Function<Context<Bind>, Ty> TmIsZero(Term t) {
+		return ctx -> subtype().subtype(visitTerm(t).apply(ctx), tyAlg().TyNat()) ? tyAlg().TyBool() : m().empty().apply(ctx);
+	}
 }
--- src/top/Typeof.java
+++ src/top/Typeof.java
@@ -1,27 +1,20 @@
-package bot;
+package top;
 
 import java.util.function.Function;
 
-import bot.tyalg.external.TyAlgMatcher;
-import bot.tyalg.shared.GTyAlg;
+import typed.termalg.shared.GTermAlg;
 import utils.Context;
 
-public interface Typeof<Term, Ty, Bind> extends top.Typeof<Term, Ty, Bind> {
+public interface Typeof<Term, Ty, Bind> extends GTermAlg<Term, Ty, Function<Context<Bind>, Ty>>, typed.Typeof<Term, Ty, Bind> {
 	Subtype<Ty> subtype();
-	@Override
-	TyAlgMatcher<Ty, Ty> tyMatcher();
-	@Override
-	GTyAlg<Ty, Ty> tyAlg();
 
 	@Override
 	default Function<Context<Bind>, Ty> TmApp(Term t1, Term t2) {
 		return ctx -> {
 			Ty ty1 = visitTerm(t1).apply(ctx);
 			Ty ty2 = visitTerm(t2).apply(ctx);
-
 			return tyMatcher()
 					.TyArr(ty11 -> ty12 -> subtype().subtype(ty2, ty11) ? ty12 : m().empty().apply(ctx))
-					.TyBot(() -> tyAlg().TyBot())
 					.otherwise(() -> m().empty().apply(ctx))
 					.visitTy(ty1);
 		};
